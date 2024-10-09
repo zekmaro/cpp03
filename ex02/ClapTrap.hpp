f37bb58e20050bc0d89ad5c03c39a6c90e3d3d18
Andrey Arama <anarama@c2r1p9.42vienna.com>
2024-10-09 18:44:55 +0200

    started ex02

---
+++ ex02/ClapTrap.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anarama <anarama@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 16:21:52 by anarama           #+#    #+#             */
+/*   Updated: 2024/10/08 18:08:00 by anarama          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+
+# define CLAPTRAP_HPP
+
+# include <string>
+
+# define DEFAULT_HEALTH 10
+# define DEFAULT_ENERGY 10
+# define DEFAULT_DAMAGE 0
+
+class ClapTrap {
+	private:
+		std::string _name;
+		int _health;
+		int _energy;
+		int _damage;
+		
+	public:
+		ClapTrap( void );
+		ClapTrap( std::string name );
+		ClapTrap( const ClapTrap& other);
+		ClapTrap& operator=(const ClapTrap& other);
+		~ClapTrap( void );
+
+		void attack( const std::string& target );
+		void takeDamage( unsigned int amount );
+		void beRepaired( unsigned int amount );
+
+		std::string	getName( void ) const;
+		int			getHealth( void ) const;
+		int			getEnergy( void ) const;
+		int			getDamage( void ) const;
+
+		void		setName( const std::string name );
+		void		setHealth( const int health );
+		void		setEnergy( const int energy );
+		void		setDamage( const int damage );
+};
+
+#endif
---
+++ ex02/FragTrap.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anarama <anarama@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/09 18:43:20 by anarama           #+#    #+#             */
+/*   Updated: 2024/10/09 18:44:42 by anarama          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_HPP
+
+# define FRAGTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class FragTrap: public ClapTrap {
+	
+};
+
+#endif //FRAGTRAP_HPP
